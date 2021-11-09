04d13c0404a58618568761e7a815ff4fe3024eac
Pete Machin <designer@deadpixelsdesign.com>
2021-11-09 21:57:54 +0000

    Add division results for user and use whole number to return the correct answers without negative or decimals

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,7 @@ function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let divNum1 = num1 * num2;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -43,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(divNum1, num2);
     } else {
         alert(`unknown game type: ${gameType}`)
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -90,6 +93,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if(operator === "/") {
+        return [operand1 - operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -134,7 +139,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
 
